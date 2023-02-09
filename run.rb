2e88aa8a5b517fc912dc4272d029fd64cf2744f5
Brian <wamwangimathenge@gmail.com>
2023-02-09 07:36:36 +0300

    Method that returns the instance variable

---
+++ run.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Recipe
+
+    # attr_reader :name
+
+    def initialize(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+
+end
+
+r1 = Recipe.new("Chicken")
+
+binding.pry
